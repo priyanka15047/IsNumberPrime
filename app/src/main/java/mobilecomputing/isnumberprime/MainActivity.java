db9cfa4f0845c649a42a39403ae765354d7c2503
Priyanka Gupta <priyanka15047@iiitd.ac.in>
2016-08-17 15:21:42 +0530

    All code

--- app/src/main/java/mobilecomputing/isnumberprime/MainActivity.java
+++ app/src/main/java/mobilecomputing/isnumberprime/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -14,29 +15,88 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "IsNumberPrime";
-    private static Button mCorrect;
-    private static Button mInCorrect;
-
-
+    private String S_NUMBER = "IN";
+    private Button mCorrect;
+    private Button mInCorrect;
+    private Button nextButton;
+    private Toast currentToast = null;
+    private TextView tv;
+    private  String st;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv  = (TextView)findViewById(R.id.TextView1);
-        tv.setText(NumberToBeChecked());
         Log.d(TAG, "In onCreate");
+        if(savedInstanceState != null){
+            st = savedInstanceState.getString(S_NUMBER);
+
+        }
+        else {
+
+            st = NumberToBeChecked();
+        }
+        tv = (TextView) findViewById(R.id.TextView1);
+        tv.setText(st);
+
 
         mCorrect = (Button)findViewById(R.id.button1);
-        mCorrect.setOnClickListener(new Button.OnClickListener()
-        {
-            public void onClick(View v)
-            {
+        mCorrect.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if (IsPrime(Double.parseDouble(st))) {
+                    if(currentToast!=null) {
+                        currentToast.cancel();
+                    }
+                    currentToast = Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT);
+                    currentToast.show();
+                } else {
+                    if(currentToast!=null) {
+                        currentToast.cancel();
+                    }
+                    currentToast = Toast.makeText(getApplicationContext(), "Oops, you are Incorrect", Toast.LENGTH_SHORT);
+                    currentToast.show();
+                }
 
             }
 
         });
+
+
+        mInCorrect = (Button)findViewById(R.id.button2);
+        mInCorrect.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if (IsPrime(Double.parseDouble(st))) {
+                    if(currentToast!=null) {
+                        currentToast.cancel();
+                    }
+                    currentToast = Toast.makeText(getApplicationContext(), "Oops, you are Incorrect", Toast.LENGTH_SHORT);
+                    currentToast.show();
+                } else {
+                    if(currentToast!=null) {
+                        currentToast.cancel();
+                    }
+                    currentToast = Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT);
+                    currentToast.show();
+                }
+
+            }
+
+        });
+
+        nextButton = (Button)findViewById(R.id.button3);
+        nextButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if(currentToast!=null) {
+                    currentToast.cancel();
+                }
+                st = NumberToBeChecked();
+                tv.setText(st);
+            }
+
+        });
+
     }
+
     public static String NumberToBeChecked()
     {
         Random rand = new Random();
@@ -45,9 +105,9 @@ public class MainActivity extends AppCompatActivity {
         return str;
     }
 
-    public static boolean IsPrime(double n)
+    public boolean IsPrime(double n)
     {
-        for(int i=2;i<Math.sqrt(n);i++)
+        for(int i=2;i<=Math.sqrt(n);i++)
         {
             if(n%i == 0)
             {
@@ -57,7 +117,13 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current game state
+        savedInstanceState.putString(S_NUMBER, st);
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+    }
 
 
     @Override
@@ -79,12 +145,14 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.d(TAG,"In onResume");
 
+
     }
 
     @Override
     public void onStop(){
         super.onStop();
         Log.d(TAG, "In onStop");
+
     }
 
     @Override
